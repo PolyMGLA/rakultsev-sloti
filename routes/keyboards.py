ec5b880ba8c49a632882bbe0ab55b112dfb6e984
PolyMGLA <timofey.blinov@icloud.com>
2025-06-10 19:35:46 +0500

    added pep8 + gifts + /user

--- routes/keyboards.py
+++ routes/keyboards.py
@@ -4,38 +4,38 @@ menu_keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
         [
             types.KeyboardButton(text="🎰Cлоты🎰"),
-            types.KeyboardButton(text="♣Блекджек🃏")
+            types.KeyboardButton(text="♣Блекджек🃏"),
         ],
         [
             # types.KeyboardButton(text="🔥Правила🔥"),
             types.KeyboardButton(text="👥Посетители👥"),
-            types.KeyboardButton(text="👾Профиль👾")
+            types.KeyboardButton(text="👾Профиль👾"),
         ],
         [
             types.KeyboardButton(text="🆘Помощь🆘"),
-            types.KeyboardButton(text="🔝Топ казино🎰")
+            types.KeyboardButton(text="🔝Топ казино🎰"),
         ],
         [
             types.KeyboardButton(text="📛Админ-панель❌"),
         ],
     ],
     resize_keyboard=True,
-    input_field_placeholder="Выберите что хотите сделать"
+    input_field_placeholder="Выберите что хотите сделать",
 )
 
 slots_keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
         [
-            types.KeyboardButton(text="✨играть✨"),
-            types.KeyboardButton(text="💲мега ласт деп💲")
+            types.KeyboardButton(text="✨крутить✨"),
+            types.KeyboardButton(text="💲мега ласт деп💲"),
         ],
         [
             types.KeyboardButton(text="🔥Правила слотов🔥"),
-            types.KeyboardButton(text="👾Профиль👾")
+            types.KeyboardButton(text="👾Профиль👾"),
         ],
         [
             types.KeyboardButton(text="🆘Помощь🆘"),
-            types.KeyboardButton(text="🔝Топ казино🎰")
+            types.KeyboardButton(text="🔝Топ казино🎰"),
         ],
         [
             types.KeyboardButton(text="🔙Назад🔙"),
@@ -45,22 +45,22 @@ slots_keyboard = types.ReplyKeyboardMarkup(
         ],
     ],
     resize_keyboard=True,
-    input_field_placeholder="Выберите что хотите сделать"
+    input_field_placeholder="Выберите что хотите сделать",
 )
 
 blackjack_keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
         [
-            types.KeyboardButton(text="✨Крутить✨"),
-            types.KeyboardButton(text="💲мега ласт деп💲")
+            types.KeyboardButton(text="✨играть✨"),
+            types.KeyboardButton(text="💲мега ласт деп💲"),
         ],
         [
             types.KeyboardButton(text="🔥Правила блэкджека🔥"),
-            types.KeyboardButton(text="👾Профиль👾")
+            types.KeyboardButton(text="👾Профиль👾"),
         ],
         [
             types.KeyboardButton(text="🆘Помощь🆘"),
-            types.KeyboardButton(text="🔝Топ казино🎰")
+            types.KeyboardButton(text="🔝Топ казино🎰"),
         ],
         [
             types.KeyboardButton(text="🔙Назад🔙"),
@@ -70,45 +70,32 @@ blackjack_keyboard = types.ReplyKeyboardMarkup(
         ],
     ],
     resize_keyboard=True,
-    input_field_placeholder="Выберите что хотите сделать"
+    input_field_placeholder="Выберите что хотите сделать",
 )
 
 admin_keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
         [
             types.KeyboardButton(text="количество участников"),
-            types.KeyboardButton(text="список участников")
+            types.KeyboardButton(text="список участников"),
         ],
         [
             types.KeyboardButton(text="посмотреть секрет"),
-            types.KeyboardButton(text="сгенерировать новый секрет")
+            types.KeyboardButton(text="сгенерировать новый секрет"),
         ],
-        [
-            types.KeyboardButton(text="тестирование")
-        ],
-        [
-            types.KeyboardButton(text="помощь админам")
-        ],
-        [
-            types.KeyboardButton(text="🔙назад🔙")
-        ],
+        [types.KeyboardButton(text="тестирование")],
+        [types.KeyboardButton(text="помощь админам")],
+        [types.KeyboardButton(text="🔙назад🔙")],
     ],
     resize_keyboard=True,
-    input_field_placeholder="Выберите что хотите сделать"
+    input_field_placeholder="Выберите что хотите сделать",
 )
 
 test_keyboard = types.ReplyKeyboardMarkup(
     keyboard=[
-        [
-            types.KeyboardButton(text="✨сосать✨")
-        ],
-        [
-            types.KeyboardButton(text="/visitors")
-        ],
-        [
-            types.KeyboardButton(text="📛Админ-панель❌")
-        ],
+        [types.KeyboardButton(text="✨сосать✨")],
+        [types.KeyboardButton(text="📛Админ-панель❌")],
     ],
     resize_keyboard=True,
-    input_field_placeholder="Выберите что хотите сделать"
+    input_field_placeholder="Выберите что хотите сделать",
 )
